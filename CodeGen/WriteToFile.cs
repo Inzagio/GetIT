e713ac9456c26a11b3a7d058716582f6c96bf94c
Trym Kristian Bjørnvik <chilled-1337@hotmail.com>
2018-08-31 14:28:48 +0200

    Added project to Source Control

---
+++ CodeGen/WriteToFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeGen
+{
+    class WriteToFile
+    {
+        public static void SaveToFile(string password)
+        {
+            var dir = @"C:\Users\TrymKBjørnvik\Documents\Test\password.txt";
+            File.WriteAllText(dir, password + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("Password saved to " + dir );
+        }
+    }
+}
