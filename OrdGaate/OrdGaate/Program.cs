50d1d100e5fbc64417f25d5186f5162e70dcfc77
Trym Kristian <chilled-1337@hotmail.com>
2018-09-11 14:17:25 +0200

    Fixed more

--- OrdGaate/OrdGaate/Program.cs
+++ OrdGaate/OrdGaate/Program.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 
 namespace OrdGaate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            var list = GetList();
+            foreach (var words in list)
+            {
+                if (words.Length > 7)
+                {
+                    Console.WriteLine(words);
+                }
+            }
+        }
+
+        //private static string DisplayWords()
+        //{
+        //    List<string> lines = GetList();
+        //    var allWords = string.Empty;
+        //    IEnumerable<string> distinctWords = lines.Distinct();
+        //    foreach (string words in distinctWords)
+        //    {
+        //        allWords = words;
+
+        //    }
+        //    Console.WriteLine(allWords);
+        //    return allWords;
+        //}
+
+        public static string[] GetList()
         {
             string path = "Ordliste.txt";
             List<string> lines = new List<string>();
@@ -20,16 +45,11 @@ namespace OrdGaate
                 {
                     lines.Add(reader.ReadLine()?.Split('\t')[1]);
                 }
-                
+
             }
 
-            IEnumerable<string> distinctWords = lines.Distinct();
-            foreach (string word in distinctWords)
-            {
-                Console.WriteLine(word);
-            }
-
-
+            var allLines = lines.Distinct().ToArray();
+            return allLines;
         }
     }
 }
