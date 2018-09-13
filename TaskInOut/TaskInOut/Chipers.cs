d6bb6c06c0e270128e00b8988d682127b5419d2c
Trym <chilled-1337@hotmail.com>
2018-09-13 11:04:26 +0200

    fixed

---
+++ TaskInOut/TaskInOut/Chipers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using static System.Array;
+
+namespace TaskInOut
+{
+    class Chipers
+    {
+        private static Random scramble = new Random();
+        private static readonly char[] Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        public static char[] chiperKey = ScrambleProperly();
+
+        public static string ChiperArray(string plainText)
+        {
+            plainText = plainText.ToUpper();
+            var outputText = string.Empty;
+            foreach (var character in plainText)
+            {
+                if (Alpha.Contains(character))
+                {
+                    //var index = Array.IndexOf(chiperKey, character);
+                    var text = chiperKey[IndexOf(Alpha, character)];
+                    if (text == character)
+                    {
+                    }
+                    outputText += text;
+                }
+                else
+                {
+                    outputText += character;
+                }
+            }
+
+
+            return outputText;
+        }
+
+        public static string DechiperArray(string plainText)
+        {
+
+            plainText = plainText.ToUpper();
+            var outputText = string.Empty;
+            foreach (var character in plainText)
+            {
+                if (chiperKey.Contains(character))
+                {
+                    //var index = Array.IndexOf(chiperKey, character);
+                    outputText += Alpha[IndexOf(chiperKey, character)];
+                }
+                else
+                {
+                    outputText += character;
+                }
+            }
+
+
+            return outputText;
+        }
+
+        public static char[] ScrambleProperly()
+        {
+            char[] outArr = Alpha.Clone() as char[];
+            for (int i = 0; i < Alpha.Length; i++)
+            {
+                if (Alpha[i] == outArr[i])
+                {
+                    var fromArr = outArr[i];
+                    var randomDigit = scramble.Next(Alpha.Length - 1);
+                    while (randomDigit == i)
+                    {
+                        randomDigit = scramble.Next(Alpha.Length - 1);
+                    }
+
+                    outArr[i] = outArr[randomDigit];
+                    outArr[randomDigit] = fromArr;
+                }
+            }
+            return outArr;
+        }
+    }
+}
--- TaskInOut/TaskInOut/Program.cs
+++ TaskInOut/TaskInOut/Program.cs
@@ -1,74 +1,23 @@
-﻿using System;
-using System.Linq;
+﻿using System.Reflection;
+using static System.Console;
+using static TaskInOut.Chipers;
 
 namespace TaskInOut
 {
     class Program
-    {   static Random scramble = new Random();
-        static char[] alpha =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        static char[] second = alpha.OrderBy(x=>scramble.Next()).ToArray();
-        
+    {
         public static void Main(string[] args)
         {
             // var intArray = new int[6];
             //int[] intArray = { 1, 2, 3, 4, 5,94,1 };
-            //Console.WriteLine(IsArraySorted.isArraySorted(intArray));
-            string plaintext = "Ibsens Ripsbaerbusker og andre buksvekster.";
-            Console.WriteLine(ChiperArray(plaintext));
-            Console.WriteLine(DechiperArray(ChiperArray(plaintext)));
+           //Console.WriteLine(IsArraySorted.isArraySorted(intArray));
+            string plaintext = "Ibsens Ripsbaerbusker og andre buskvekster.";
+            WriteLine(ChiperArray(plaintext));
+            WriteLine(DechiperArray(ChiperArray(plaintext)));
+            WriteLine(chiperKey);
+            var dir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\chiperKey.txt";
+            if (chiperKey != null) System.IO.File.WriteAllText(dir, new string(chiperKey));
         }
-
-        public static string ChiperArray(string plainText)
-        {
-            plainText = plainText.ToUpper();
-            var outputText = string.Empty;
-            foreach (var character in plainText)
-            {
-                if (alpha.Contains(character))
-                {
-                    //var index = Array.IndexOf(second, character);
-                    var text = second[Array.IndexOf(alpha, character)];
-                    if (text == character)
-                    {
-                        
-                    }
-                        outputText += text;
-
-
-                }
-                else
-                {
-                    outputText += character;
-                }
-            }
-
-
-            return outputText;
-        }
-        public static string DechiperArray(string plainText)
-        {
-
-            plainText = plainText.ToUpper();
-            var outputText = string.Empty;
-            foreach (var character in plainText)
-            {
-                if (second.Contains(character))
-                {
-                    //var index = Array.IndexOf(second, character);
-                    outputText += alpha[Array.IndexOf(second, character)];
-                }
-                else
-                {
-                    outputText += character;
-                }
-            }
-
-
-            return outputText;
-        }
-
-
-
     }
 
 }
