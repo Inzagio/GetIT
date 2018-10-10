1b9bf4960a96097dbb491971486578858b5acd9b
Trym Kristian <chilled-1337@hotmail.com>
2018-10-10 22:50:04 +0200

    Doing some more stuff

---
+++ TestingDapperIGuess/TestingDapperIGuess/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDapperIGuess
+{
+    public  class Person
+    {
+        public int Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string EmailAddress { get; }
+        public string PhoneNumber { get; }
+
+
+    }
+}
